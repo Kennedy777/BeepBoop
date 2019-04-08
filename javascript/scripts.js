706b2bbd62c07baf5575e122c2f567d1883fdedd
Marguerite Kennedy <reach.marguerite@gmail.com>
2019-04-08 07:12:20 -0700

    update business logic

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-  $("#formOne").submit(function(event) {
-  event.preventDefault();
+  $(document).ready(function(){
+    $("#formHal").submit(function(event) {
+    event.preventDefault();
 
-  var input = $("userNumber").val();
-   var Array1 ="" convertUserInput(inputNumber);
-   var result = "";
-    for (var i = 0; i <=input; i++) {
-    result += Array1[i] + " ";
+    var input =$("#userNum").val();
+    $("#formHal")[0].reset();
+      $("#computerSpeak").text(goNum(input));
+      $("#computerSpeak").show();
+      });
+    });
+    function goNum(input){
+     var Array1 = [];
+        for (var i = 0; i <=input; i++) {
+      var Array1 = i.toString().split('');
+      if(Array1.includes("3")){
+        Array1.push("I'm sorry, Dave, I can't do that.");
+      } else if(Array1.includes("2")){
+        Array1.push("Boop!");
+      } else if)Array1.includes("1")){
+        Array1.push("Beep!");
+      } else {
+        Array1.push(i);
+      }
     }
-$("#response").html(response);
-  });
-});
+      return Array1[Array1.length-1];
+  );
